9b2074a573dbe92a312793e6b28bc07d6264eda7
Ole Krüger <git@ole.lol>
2023-02-03 16:47:00 +0000

    Support other apps in skhd

--- skhd.nix
+++ skhd.nix
@@ -1,9 +1,42 @@
-{pkgs, ...}: {
-  home = {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.ole = {
+    browserApp = lib.mkOption {
+      type = lib.types.str;
+
+      default = ''/Applications/Brave\ Browser.app'';
+    };
+
+    hasSlack = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+
+  config.home = {
     packages = [pkgs.skhd];
 
     file.".skhdrc" = {
-      source = ./.skhdrc;
+      text = ''
+        cmd - 1 [
+            "wezterm" ~
+            * : open -a /Applications/WezTerm.app
+        ]
+
+        cmd - 2 [
+            "wezterm" ~
+            * : open -a /Applications/WezTerm.app
+        ]
+
+        cmd - 3 : open -a ${config.ole.browserApp}
+
+        ${lib.optionalString config.ole.hasSlack "cmd - 4 : open -a /Applications/Slack.app"}
+      '';
+
       onChange = ''
         ${pkgs.skhd}/bin/skhd -r
       '';
