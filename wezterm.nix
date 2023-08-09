5712db4a0037bcd267a783bf4c88d29e1c28c8be
Ole Krüger <git@ole.lol>
2023-08-09 13:04:45 +0100

    Automatically switch WezTerm colour scheme when OS appearance changes

--- wezterm.nix
+++ wezterm.nix
@@ -58,6 +58,24 @@
     extraConfig = ''
       local wezterm = require('wezterm')
 
+      function scheme_for_appearance(appearance)
+        if appearance:find("Dark") then
+          return "ayu"
+        else
+          return "ayu_light"
+        end
+      end
+
+      wezterm.on("window-config-reloaded", function(window, pane)
+        local overrides = window:get_config_overrides() or {}
+        local appearance = window:get_appearance()
+        local scheme = scheme_for_appearance(appearance)
+        if overrides.color_scheme ~= scheme then
+          overrides.color_scheme = scheme
+          window:set_config_overrides(overrides)
+        end
+      end)
+
       return {
         font = wezterm.font('${config.ole.terminal.font}'),
         font_size = ${builtins.toString config.ole.terminal.fontSize},
