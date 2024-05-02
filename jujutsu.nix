910670d3ba7a9d94f991bcbbf2bb6b8fceeffe6d
Ole Krüger <git@ole.lol>
2024-05-02 18:17:26 +0100

    Jujutsu: Print logs nicely

--- jujutsu.nix
+++ jujutsu.nix
@@ -20,9 +20,44 @@
       signing.backend = "gpg";
       signing.key = "51462186765EF78CA1560BF192FC24B5225314A9";
 
+      templates = {
+        log = "change_comfortable";
+      };
+
       template-aliases = {
         "format_short_id(id)" = "id.shortest()";
         "format_timestamp(timestamp)" = "timestamp.ago()";
+        # "format_short_signature(signature)" = "coalesce(signature.name(), email_placeholder)";
+
+        change_compact = ''
+          if(root,
+            format_root_commit(self),
+            label(if(current_working_copy, "working_copy"),
+              concat(
+                separate(" ",
+                  format_short_change_id_with_hidden_and_divergent_info(self),
+                  if(empty, label("empty", "(empty)")),
+                  if(conflict, label("conflict", "conflict")),
+                  branches,
+                  tags,
+                  working_copies,
+                  git_head,
+                  format_short_commit_id(commit_id),
+                  format_timestamp(committer.timestamp()),
+                  format_short_signature(author),
+                ) ++ "\n",
+                separate(" ",
+                  if(description,
+                    description.first_line(),
+                    label(if(empty, "empty"), description_placeholder),
+                  ),
+                ) ++ "\n",
+              ),
+            )
+          )
+        '';
+
+        change_comfortable = ''change_compact ++ "\n"'';
       };
 
       aliases = {
