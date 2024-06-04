1830370d280d02fd08600ab93ad36b977637d752
Ole Krüger <git@ole.lol>
2024-06-04 11:40:50 +0100

    NeoVim: Detect indentation

--- neovim/init.lua
+++ neovim/init.lua
@@ -56,6 +56,13 @@ require("lazy").setup({
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				indent = {
+					enable = true
+				}
+			})
+		end
 	},
 
 	{
