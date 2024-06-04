8f74b31fc3a6aa317d7e72abcd09510053ea7b24
Ole Krüger <git@ole.lol>
2024-06-04 16:29:25 +0100

    NeoVim: Add more useful shortcuts for LSP stuff

--- neovim/init.lua
+++ neovim/init.lua
@@ -175,6 +175,8 @@ require("lazy").setup({
 -- Keys
 vim.keymap.set("", "<Space><Space>q", "<cmd>qa<cr>")
 vim.keymap.set("", "<Space>k", vim.lsp.buf.hover)
+vim.keymap.set("", "<Space>a", vim.lsp.buf.code_action)
+vim.keymap.set("", "<Space>r", vim.lsp.buf.rename)
 vim.keymap.set("", "<Space>c", "<cmd>bd<cr>")
 vim.keymap.set("", "<Space>y", '"+y')
 vim.keymap.set("", "<Space>p", '"+p')
