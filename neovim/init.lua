8336699893a77a202386ca954c1e99ae4da88ab3
Ole Krüger <git@ole.lol>
2024-06-25 10:38:26 +0100

    NeoVim: Auto-resize splits

--- neovim/init.lua
+++ neovim/init.lua
@@ -352,3 +352,9 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
 		vim.cmd("setlocal nonumber")
 	end
 })
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	callback = function()
+		vim.cmd.wincmd("=")
+	end
+})
