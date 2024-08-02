ef1b8ffef98f7836eae8c2279cc7e8ed0cecec77
Ole Krüger <git@ole.lol>
2024-08-02 16:34:49 +0100

    Use TaskWarrior TUI with config fixes

--- common-packages.nix
+++ common-packages.nix
@@ -1,5 +1,19 @@
 {pkgs, ...}:
-with pkgs; {
+with pkgs; let
+  taskwarrior-tui-fixed = pkgs.taskwarrior-tui.overrideAttrs (oldAttrs: rec {
+    src = pkgs.fetchFromGitHub {
+      owner = "vapourismo";
+      repo = "taskwarrior-tui";
+      rev = "115fe6242f18752a326fab4d155437d6f37db9aa";
+      sha256 = "ZWJANDVwLnT5woQAna+tm4WR1wRejqN+L9xkHxHgBGE=";
+    };
+
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (_: {
+      inherit src;
+      outputHash = "sha256-afRFYyOaFZBbOZp7KZXIRlYhdc7J0bFEdOdYkw0gPlY=";
+    });
+  });
+in {
   home.packages =
     [
       alejandra
@@ -18,7 +32,7 @@ with pkgs; {
       ripgrep
       stgit
       taskwarrior3
-      taskwarrior-tui
+      taskwarrior-tui-fixed
       tree
       vim
       watchman
