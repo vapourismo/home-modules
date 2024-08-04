69037d96e5b004cecdf973aacc13388fed817166
Ole Krüger <git@ole.lol>
2024-08-04 13:59:21 +0100

    NeoVim: Filter buffer selector by cwd

--- neovim/init.lua
+++ neovim/init.lua
@@ -131,7 +131,8 @@ require("lazy").setup({
 
 			vim.keymap.set("n", "<Space>f", builtin.find_files)
 			vim.keymap.set("n", "<Space>/", builtin.live_grep)
-			vim.keymap.set("n", "<Space>b", builtin.buffers)
+			vim.keymap.set("n", "<Space>b", function() builtin.buffers({ only_cwd = true }) end)
+			vim.keymap.set("n", "<Space>B", builtin.buffers)
 			vim.keymap.set("n", "<Space>s", builtin.lsp_document_symbols)
 			vim.keymap.set("n", "<Space>S", builtin.lsp_dynamic_workspace_symbols)
 			vim.keymap.set("n", "<Space>D", builtin.diagnostics)
