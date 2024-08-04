9e7431a0cc795e1e597469a1674aded2feffb1f9
Ole Krüger <git@ole.lol>
2024-08-04 22:15:11 +0100

    Flake: Upgrade nixpkgs and co

--- common-packages.nix
+++ common-packages.nix
@@ -1,6 +1,25 @@
 {pkgs, ...}:
 with pkgs; let
+  taskwarrior3-fixed = pkgs.taskwarrior3.overrideAttrs (oldAttrs: rec {
+    version = "r5c6cc3e5229676655e93264e24a146a26b980e45";
+    src = pkgs.fetchFromGitHub {
+      owner = "GothenburgBitFactory";
+      repo = "taskwarrior";
+      rev = "5c6cc3e5229676655e93264e24a146a26b980e45";
+      fetchSubmodules = true;
+      sha256 = "iKpOExj1xM9rU/rIcOLLKMrZrAfz7y9X2kt2CjfMOOQ=";
+    };
+
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (_: {
+      inherit src;
+      outputHash = "sha256-WE/3lmgZg9qztaMopB8IiUz/MzFNqk4uYCShsNYFCBw=";
+    });
+
+    doCheck = false;
+  });
+
   taskwarrior-tui-fixed = pkgs.taskwarrior-tui.overrideAttrs (oldAttrs: rec {
+    version = "r115fe6242f18752a326fab4d155437d6f37db9aa";
     src = pkgs.fetchFromGitHub {
       owner = "vapourismo";
       repo = "taskwarrior-tui";
@@ -31,7 +50,7 @@ in {
       nixos-rebuild
       ripgrep
       stgit
-      taskwarrior3
+      taskwarrior3-fixed
       taskwarrior-tui-fixed
       tree
       vim
