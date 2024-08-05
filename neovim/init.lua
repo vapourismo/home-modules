14cdc84bcfd134c8122ccccd5f6cf9c7bd02e499
Ole Krüger <git@ole.lol>
2024-08-05 11:04:43 +0100

    NeoVim: Configure terminal colors scheme

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,7 +2,6 @@
 local data_dir = vim.fn.stdpath("data")
 
 -- Config
-vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.compatible = false
 vim.opt.tabstop = 4
@@ -17,6 +16,45 @@ vim.opt.cursorline = true
 vim.opt.sessionoptions = "curdir,folds,globals,help,tabpages,terminal,winsize"
 vim.go.ignorecase = true
 vim.o.showtabline = 2
+
+-- Terminal colors
+vim.opt.termguicolors = true
+
+-- Ayu
+vim.g.terminal_color_0 = "#000000"
+vim.g.terminal_color_1 = "#ff3333"
+vim.g.terminal_color_2 = "#b8cc52"
+vim.g.terminal_color_3 = "#e7c547"
+vim.g.terminal_color_4 = "#36a3d9"
+vim.g.terminal_color_5 = "#f07178"
+vim.g.terminal_color_6 = "#95e6cb"
+vim.g.terminal_color_7 = "#ffffff"
+vim.g.terminal_color_8 = "#323232"
+vim.g.terminal_color_9 = "#ff6565"
+vim.g.terminal_color_10 = "#eafe84"
+vim.g.terminal_color_11 = "#fff779"
+vim.g.terminal_color_12 = "#68d5ff"
+vim.g.terminal_color_13 = "#ffa3aa"
+vim.g.terminal_color_14 = "#c7fffd"
+vim.g.terminal_color_15 = "#ffffff"
+
+-- Everforest Light
+-- vim.g.terminal_color_0 = "#5C6A72"
+-- vim.g.terminal_color_1 = "#F85552"
+-- vim.g.terminal_color_2 = "#8DA101"
+-- vim.g.terminal_color_3 = "#DFA000"
+-- vim.g.terminal_color_4 = "#3A94C5"
+-- vim.g.terminal_color_5 = "#DF69BA"
+-- vim.g.terminal_color_6 = "#35A77C"
+-- vim.g.terminal_color_7 = "#DFDDC8"
+-- vim.g.terminal_color_8 = "#2E383C"
+-- vim.g.terminal_color_9 = "#E67E80"
+-- vim.g.terminal_color_10 = "#A7C080"
+-- vim.g.terminal_color_11 = "#DBBC7F"
+-- vim.g.terminal_color_12 = "#7FBBB3"
+-- vim.g.terminal_color_13 = "#D699B6"
+-- vim.g.terminal_color_14 = "#83C092"
+-- vim.g.terminal_color_15 = "#D3C6AA"
 
 -- Disable netrw
 vim.g.loaded_netrw = 1
