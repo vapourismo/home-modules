b1ce25c0989ab72bee7734888bc8a559a0b469c9
Ole Krüger <git@ole.lol>
2024-08-21 21:46:22 +0100

    NeoVim: Enable buffer scoping

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -30,8 +30,8 @@ return {
 
 		vim.keymap.set("n", "<Space>f", builtin.find_files)
 		vim.keymap.set("n", "<Space>/", builtin.live_grep)
-		vim.keymap.set("n", "<Space>b", function() builtin.buffers({ only_cwd = true }) end)
-		vim.keymap.set("n", "<Space>B", builtin.buffers)
+		vim.keymap.set("n", "<Space>b", builtin.buffers)
+		vim.keymap.set("n", "<Space>B", function() vim.cmd("Telescope scope buffers") end)
 		vim.keymap.set("n", "<Space>s", builtin.lsp_document_symbols)
 		vim.keymap.set("n", "<Space>S", builtin.lsp_dynamic_workspace_symbols)
 		vim.keymap.set("n", "<Space>D", builtin.diagnostics)
