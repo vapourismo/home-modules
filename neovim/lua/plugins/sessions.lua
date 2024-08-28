997bee1a988e0fded75df6381fe1b8ec7627e85b
Ole Krüger <git@ole.lol>
2024-08-28 22:27:42 +0100

    NeoVim: Bind session saving and loading

--- neovim/lua/plugins/sessions.lua
+++ neovim/lua/plugins/sessions.lua
@@ -6,6 +6,11 @@ return {
 			"nvim-telescope/telescope.nvim",
 			{ "stevearc/dressing.nvim", opts = {} },
 		},
+		keys = {
+			{ "<Space><Space>O", "<cmd>Autosession search<cr>" },
+			{ "<Space><Space>s", "<cmd>SessionSave<cr>" },
+			{ "<Space><Space>o", "<cmd>SessionSearch<cr>" },
+		},
 		opts = {
 			suppressed_dirs = { "~", "~/Workspaces", "~/Downloads", "/" },
 		}
