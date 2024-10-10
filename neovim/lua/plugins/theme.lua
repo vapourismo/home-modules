949eb38512fa46c4d8c69d6ad14c6f1100414b74
Ole Krüger <git@ole.lol>
2024-10-10 14:30:05 +0100

    NeoVim: Add Noice again

--- neovim/lua/plugins/theme.lua
+++ neovim/lua/plugins/theme.lua
@@ -66,5 +66,32 @@ return {
 				vim.g.terminal_color_15 = "#D3C6AA"
 			end,
 		},
+	},
+
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			{
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				presets = {
+					bottom_search = true,
+					command_palette = true,
+					long_message_to_split = true,
+					inc_rename = false,
+					lsp_doc_border = false,
+				},
+			}
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		}
 	}
 }
