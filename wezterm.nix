3ccd31b28a564f82751ed9329978cb5c70e6273c
Ole Krüger <git@ole.lol>
2024-10-12 14:13:33 +0100

    Update flake inputs

--- wezterm.nix
+++ wezterm.nix
@@ -19,41 +19,7 @@
   config.programs.wezterm = {
     enable = true;
 
-    package = let
-      version = "20230712-072601-f4abf8fd";
-      src = pkgs.fetchFromGitHub {
-        owner = "wez";
-        repo = "wezterm";
-        rev = version;
-        fetchSubmodules = true;
-        sha256 = "B6AakLbTWIN123qAMQk/vFN83HHNRSNkqicNRU1GaCc=";
-      };
-    in
-      pkgs.wezterm.override {
-        rustPlatform =
-          pkgs.rustPlatform
-          // {
-            buildRustPackage = args:
-              pkgs.rustPlatform.buildRustPackage (
-                if args.pname == "wezterm"
-                then
-                  builtins.removeAttrs args ["cargoHash" "cargoSha256" "cargoLock"]
-                  // {
-                    inherit src version;
-                    patches = [];
-                    cargoLock = {
-                      lockFile = src + /Cargo.lock;
-                      outputHashes = {
-                        "image-0.24.5" = "sha256-fTajVwm88OInqCPZerWcSAm1ga46ansQ3EzAmbT58Js=";
-                        "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
-                        "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
-                      };
-                    };
-                  }
-                else args
-              );
-          };
-      };
+    package = pkgs.wezterm;
 
     extraConfig = ''
       local wezterm = require('wezterm')
