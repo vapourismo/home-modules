7ed996cd66b3b8d242f3c4c7d5308de1ec211001
Ole Krüger <git@ole.lol>
2024-10-27 11:43:33 +0000

    Switch to Brave on personal

--- personal.nix
+++ personal.nix
@@ -14,7 +14,7 @@
   ];
 
   ole = {
-    slot3 = "/Applications/Arc.app";
+    slot3 = "/Applications/Brave Browser.app";
     slot4 = "/Applications/Todoist.app";
     jj.sshSignKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7vlc902QXTseSF7NsFy3CouUnWFQWDFy1EvS0CRD5q";
   };
