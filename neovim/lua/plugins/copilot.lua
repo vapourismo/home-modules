5e309669b423dd0765cb83abee7b77d3f7ade379
Ole Krüger <git@ole.lol>
2024-11-02 23:17:23 +0000

    Add config for Avante

--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -1,12 +1,37 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = {
-		suggestion = {
-			enabled = true,
-			auto_trigger = true,
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = {
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+			},
+			panel = { enabled = false },
+		}
+	},
+
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false,
+		opts = {},
+		build = "make",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
 		},
-		panel = { enabled = false },
+		enabled = false
 	}
 }
