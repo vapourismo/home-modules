5515cbfa44a1731dd11099911abb286f395ca740
Ole Krüger <git@ole.lol>
2024-11-29 12:06:02 +0000

    NeoVim: Check all features in Rust LSP

--- neovim/lua/plugins/lsp-configs.lua
+++ neovim/lua/plugins/lsp-configs.lua
@@ -1,30 +1,30 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
 
-		lspconfig.rust_analyzer.setup({
-			inlay_hints = { enabled = true },
-			settings = {
-				["rust-analyzer"] = {
-					check = { command = "clippy" },
-					cargo = { features = "all" },
-					imports = { granularity = { enforce = true } },
-				}
-			}
-		})
-		lspconfig.lua_ls.setup({})
-		lspconfig.nil_ls.setup({
-			settings = {
-				["nil"] = {
-					formatting = { command = { "alejandra" } }
-				}
-			}
-		})
-		lspconfig.taplo.setup({})
-		lspconfig.ocamllsp.setup({})
-		lspconfig.pylsp.setup({})
-		lspconfig.gopls.setup({})
-		lspconfig.openscad_lsp.setup({})
-	end
+        lspconfig.rust_analyzer.setup({
+            inlay_hints = { enabled = true },
+            settings = {
+                ["rust-analyzer"] = {
+                    check = { command = "clippy", features = "all" },
+                    cargo = { features = "all" },
+                    imports = { granularity = { enforce = true } },
+                }
+            }
+        })
+        lspconfig.lua_ls.setup({})
+        lspconfig.nil_ls.setup({
+            settings = {
+                ["nil"] = {
+                    formatting = { command = { "alejandra" } }
+                }
+            }
+        })
+        lspconfig.taplo.setup({})
+        lspconfig.ocamllsp.setup({})
+        lspconfig.pylsp.setup({})
+        lspconfig.gopls.setup({})
+        lspconfig.openscad_lsp.setup({})
+    end
 }
