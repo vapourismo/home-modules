d6ecb7ef030a142547a5f131a4da67c30f789052
Ole Krüger <git@ole.lol>
2024-12-18 00:20:07 +0000

    NeoVim: Display TS context

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,11 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			indent = {
-				enable = true
-			}
-		})
-	end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				indent = {
+					enable = true
+				}
+			})
+		end
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			multiwindow = true,
+		}
+	}
 }
