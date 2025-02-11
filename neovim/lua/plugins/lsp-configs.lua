5512123e2064b4afe6ea4ea602e6f4fae6f23b3e
Ole Krüger <git@ole.lol>
2025-02-11 23:40:39 +0000

    NeoVim: RA: Allow custom configuration to be stored in the root dir

--- neovim/lua/plugins/lsp-configs.lua
+++ neovim/lua/plugins/lsp-configs.lua
@@ -11,7 +11,23 @@ return {
 					cargo = { features = "all" },
 					imports = { granularity = { enforce = true } },
 				}
-			}
+			},
+			on_new_config = function(new_config, new_root_dir)
+				local ra_config_file = io.open(new_root_dir .. "/.rust-analyzer.json", "r")
+				if not ra_config_file then
+					return
+				end
+
+				local ra_config = ra_config_file:read("*a")
+				ra_config_file:close()
+				ra_config = vim.json.decode(ra_config)
+
+				new_config.settings["rust-analyzer"] = vim.tbl_deep_extend(
+					"force",
+					new_config.settings["rust-analyzer"],
+					ra_config
+				)
+			end,
 		})
 		lspconfig.lua_ls.setup({})
 		lspconfig.nil_ls.setup({
