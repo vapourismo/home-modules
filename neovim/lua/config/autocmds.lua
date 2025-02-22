395581bd63f1e41abf8d16e7051cb5b69736b730
Ole Krüger <git@ole.lol>
2025-02-22 11:45:04 +0000

    NeoVim: Automatically re-balance all windows

--- neovim/lua/config/autocmds.lua
+++ neovim/lua/config/autocmds.lua
@@ -1,77 +1,77 @@
 -- Remove line numbers and sign column in terminal buffers
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
-    callback = function()
-        vim.cmd.setlocal("nonumber")
-        vim.cmd.setlocal("norelativenumber")
-        vim.cmd.setlocal("signcolumn=no")
-    end,
+	callback = function()
+		vim.cmd.setlocal("nonumber")
+		vim.cmd.setlocal("norelativenumber")
+		vim.cmd.setlocal("signcolumn=no")
+	end,
 })
 
 -- Format on save
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = {
-        "*.rs",
-        "*.nix",
-        "*.lua",
-        "*.toml",
-        "*.mli",
-        "*.ml",
-    },
-    callback = function()
-        vim.lsp.buf.format()
-    end,
+	pattern = {
+		"*.rs",
+		"*.nix",
+		"*.lua",
+		"*.toml",
+		"*.mli",
+		"*.ml",
+	},
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 
 -- Enable inlay hints for LSP
 vim.api.nvim_create_autocmd({ "LspAttach" }, {
-    callback = function()
-        vim.lsp.inlay_hint.enable(true)
-    end,
+	callback = function()
+		vim.lsp.inlay_hint.enable(true)
+	end,
 })
 
 -- Automaticaly equalise window sizes when the terminal is resized
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-    callback = function()
-        vim.cmd.wincmd("=")
-    end,
+vim.api.nvim_create_autocmd({ "VimResized", "WinNew", "WinResized" }, {
+	callback = function()
+		vim.cmd.wincmd("=")
+	end,
 })
 
 -- Automatically close the buffer when it gets hidden for Git and Jujutsu message buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = {
-        "gitcommit",
-        "gitrebase",
-        "gitconfig",
-        "addp-hunk-edit.diff",
-        "jj",
-    },
-    callback = function()
-        vim.cmd("set bufhidden=delete")
-    end,
+	pattern = {
+		"gitcommit",
+		"gitrebase",
+		"gitconfig",
+		"addp-hunk-edit.diff",
+		"jj",
+	},
+	callback = function()
+		vim.cmd("set bufhidden=delete")
+	end,
 })
 
 -- Remote control server
 vim.api.nvim_create_autocmd({ "UIEnter" }, {
-    callback = function()
-        vim.fn.serverstart("/tmp/nvimsocket")
-    end,
+	callback = function()
+		vim.fn.serverstart("/tmp/nvimsocket")
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
-    callback = function()
-        vim.fn.serverstop("/tmp/nvimsocket")
-    end,
+	callback = function()
+		vim.fn.serverstop("/tmp/nvimsocket")
+	end,
 })
 
 -- Active window
 vim.api.nvim_create_autocmd({ "WinEnter" }, {
-    callback = function()
-        vim.wo.cursorline = true
-    end,
+	callback = function()
+		vim.wo.cursorline = true
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "WinLeave" }, {
-    callback = function()
-        vim.wo.cursorline = false
-    end,
+	callback = function()
+		vim.wo.cursorline = false
+	end,
 })
