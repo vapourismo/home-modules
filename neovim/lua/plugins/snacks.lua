58f308b4ca6e0d83b8a4b2d9697391276e062c5d
Ole Krüger <git@ole.lol>
2025-02-28 23:10:19 +0000

    NeoVim: Snacks: Use nice floaty terminals

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -5,5 +5,7 @@ return {
 	opts = {},
 	keys = {
 		{ "<Space>z", function() Snacks.zen() end },
+		{ "<C-;>",    function() Snacks.terminal(vim.o.shell) end, mode = { "n", "t", "v" } },
+		{ "<C-\\>",   function() Snacks.terminal() end,            mode = { "n", "t", "v" } },
 	},
 }
