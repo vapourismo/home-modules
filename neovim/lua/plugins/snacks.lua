21cdf1d07d7d0e1cdb3e0c08e214fa110600e126
Ole Krüger <git@ole.lol>
2025-03-01 00:05:53 +0000

    NeoVim: Style Zen mode

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -2,7 +2,21 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	opts = {},
+	opts = {
+		zen = {
+			toggles = {
+				dim = false
+			},
+			show = {
+				statusline = true
+			}
+		},
+		styles = {
+			zen = {
+				width = 140
+			},
+		}
+	},
 	keys = {
 		{ "<Space>z", function() Snacks.zen() end },
 		{ "<C-;>",    function() Snacks.terminal(vim.o.shell, { cwd = vim.fn.getcwd(-1, 0) }) end, mode = { "n", "t", "v" } },
