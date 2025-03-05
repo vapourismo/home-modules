33a953f1a3fe4698753dd5da12de9ed6595f87b3
Ole Krüger <git@ole.lol>
2025-03-05 20:53:56 +0000

    NeoVim: Make DirenvLoad more verbose

--- neovim/lua/config/usercmds.lua
+++ neovim/lua/config/usercmds.lua
@@ -1,20 +1,36 @@
 -- Direnv Support
 vim.api.nvim_create_user_command("DirenvLoad", function()
+	local notification
+	local notification_title = "Direnv"
+
+	local function notify(msg, level)
+		notification = vim.notify(msg, level, { title = notification_title, replace = notification })
+	end
+
 	vim.system(
 		{ "direnv", "exec", "/", "direnv", "export", "json" },
 		{ text = true },
 		vim.schedule_wrap(function(result)
+			notify("Loading direnv", vim.log.levels.INFO)
+
 			if result.code ~= 0 then
-				vim.notify("Failed to load direnv:\n" .. result.stderr, vim.log.levels.ERROR)
+				notify("Failed to load direnv:\n" .. result.stderr, vim.log.levels.ERROR)
 				return
 			end
+
+			if not result.stdout or result.stdout == "" then
+				notify("No direnv set up", vim.log.levels.ERROR)
+				return
+			end
+
+			vim.print(result.stdout)
 
 			local env = vim.fn.json_decode(result.stdout)
 			for key, value in pairs(env) do
 				vim.env[key] = value
 			end
 
-			vim.notify("Loaded direnv variables", vim.log.levels.INFO)
+			notify("Loaded direnv variables", vim.log.levels.INFO)
 		end)
 	)
 end, {})
