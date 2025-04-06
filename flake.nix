c46e989002fc54d6d99d9a1dcf974a962d6fef08
Ole Krüger <git@ole.lol>
2025-04-06 22:30:38 +0100

    Re-organise Home modules for Mac profiles

--- flake.nix
+++ flake.nix
@@ -13,16 +13,51 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
     ...
   } @ inputs: {
+    homeModules = {
+      cargo = import ./cargo.nix;
+      common-packages = import ./common-packages.nix;
+      darwin-defaults = import ./darwin-defaults.nix;
+      git = import ./git.nix;
+      home = import ./home.nix;
+      jujutsu = import ./jujutsu.nix;
+      neovim = import ./neovim.nix;
+      nixpkgs = import ./nixpkgs.nix;
+      skhd = import ./skhd.nix;
+      terminal = import ./terminal.nix;
+    };
+
     homeConfigurations = {
       personal = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."aarch64-darwin";
 
         modules = [
-          ./personal.nix
+          self.homeModules.cargo
+          self.homeModules.common-packages
+          self.homeModules.darwin-defaults
+          self.homeModules.git
+          self.homeModules.home
+          self.homeModules.jujutsu
+          self.homeModules.neovim
+          self.homeModules.nixpkgs
+          self.homeModules.skhd
+          self.homeModules.terminal
+
+          {
+            home = {
+              username = "ole";
+              stateVersion = "23.05";
+            };
+
+            ole = {
+              slot3 = "/Applications/Orion.app";
+              jj.sshSignKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7vlc902QXTseSF7NsFy3CouUnWFQWDFy1EvS0CRD5q";
+            };
+          }
         ];
 
         extraSpecialArgs = {
@@ -34,7 +69,34 @@
         pkgs = nixpkgs.legacyPackages."aarch64-darwin";
 
         modules = [
-          ./work.nix
+          self.homeModules.cargo
+          self.homeModules.common-packages
+          self.homeModules.darwin-defaults
+          self.homeModules.git
+          self.homeModules.home
+          self.homeModules.jujutsu
+          self.homeModules.neovim
+          self.homeModules.nixpkgs
+          self.homeModules.skhd
+          self.homeModules.terminal
+
+          {
+            home = {
+              username = "ole";
+              stateVersion = "23.05";
+            };
+
+            ole = {
+              slot3 = "/Applications/Orion.app";
+              slot4 = "/Applications/Slack.app";
+              slot5 = "/Applications/Linear.app";
+              jj = {
+                gpgSignKey = "E2EF5D72A6FA996563FD44059F593788E8F596AC";
+                email = "ole.kruger@trili.tech";
+              };
+              sccache = false;
+            };
+          }
         ];
 
         extraSpecialArgs = {
