080507c7bf3d94c57dec370df02f3c6cb85d6ed2
Ole Krüger <git@ole.lol>
2025-06-25 16:40:57 +0100

    NeoVim: Telescope: Streamline symbol pickers

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -22,14 +22,25 @@ return {
 						["<C-j>"] = actions.move_selection_next,
 						["<C-k>"] = actions.move_selection_previous,
 					}
-				}
+				},
 			}
 		})
 
 		telescope.load_extension("fzf")
 
 		vim.keymap.set("n", "<Space>B", function() vim.cmd("Telescope scope buffers") end)
-		vim.keymap.set("n", "<Space>s", builtin.lsp_document_symbols)
-		vim.keymap.set("n", "<Space>S", builtin.lsp_dynamic_workspace_symbols)
+		vim.keymap.set("n", "<Space>s", function()
+			builtin.lsp_document_symbols({
+				layout_strategy = "center",
+				symbol_width = 100,
+			})
+		end)
+		vim.keymap.set("n", "<Space>S", function()
+			builtin.lsp_dynamic_workspace_symbols({
+				layout_strategy = "vertical",
+				fname_width = 150,
+				symbol_width = 100,
+			})
+		end)
 	end
 }
