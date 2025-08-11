f0621cbb2e47a3eb8d7b9c731df8feeed43c9472
Ole Krüger <git@ole.lol>
2025-08-11 20:23:20 +0100

    JJ: Upgrade to v0.32.0

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       url = "github:chisui/zsh-nix-shell";
       flake = false;
     };
-    jujutsu.url = "github:jj-vcs/jj/v0.31.0";
+    jujutsu.url = "github:jj-vcs/jj/v0.32.0";
     jjui.url = "github:idursun/jjui/v0.8.12";
     mergiraf.url = "git+https://codeberg.org/mergiraf/mergiraf.git?ref=refs/tags/v0.11.0";
   };
