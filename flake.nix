9d94b46ec0bd117f3547b499d18e99f58bdfc49f
Ole Krüger <git@ole.lol>
2025-11-17 10:22:18 +0000

    Bump Flake inputs to Nix 25.05 for Neovim 0.11.5

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     omz-nix-shell-plugin = {
