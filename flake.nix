e3487e8f93b7842ea4c9c053920bb0ac8f9d99d6
Ole Krüger <git@ole.lol>
2025-11-17 23:21:20 +0000

    Make dev1 an Arm64 machine

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
         };
 
         dev1 = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          pkgs = nixpkgs.legacyPackages."aarch64-linux";
 
           modules = [
             self.homeModules.cargo
