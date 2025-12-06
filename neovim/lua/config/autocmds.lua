cab0438d3432756a6913d67e447fcf2375638f6e
Ole Krüger <git@ole.lol>
2025-12-06 16:38:10 +0000

    NeoVim: Tweak LSP configs

--- neovim/lua/config/autocmds.lua
+++ neovim/lua/config/autocmds.lua
@@ -16,9 +16,11 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		"*.toml",
 		"*.mli",
 		"*.ml",
+		"*.ts",
+		"*.tsx",
 	},
 	callback = function()
-		vim.lsp.buf.format()
+		vim.lsp.buf.format({ async = true })
 	end,
 })
 
