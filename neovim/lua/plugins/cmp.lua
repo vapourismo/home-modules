8d54e98ed29c01ca6aaaef3da5f4cd783534d3b2
Ole Krüger <git@ole.lol>
2026-01-19 22:39:22 +0000

    NeoVim: Make completion key bindings more consistent

--- neovim/lua/plugins/cmp.lua
+++ neovim/lua/plugins/cmp.lua
@@ -1,67 +1,100 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "zbirenbaum/copilot.lua",
-  },
-  config = function()
-    local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"zbirenbaum/copilot.lua",
+	},
+	config = function()
+		local cmp = require("cmp")
 
-    cmp.setup({
-      preselect = cmp.PreselectMode.None,
-      mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<Enter>"] = cmp.mapping.confirm({
-          select = false
-        }),
-        ["<Tab>"] = cmp.mapping.select_next_item({
-          behavior = cmp.SelectBehavior.Insert
-        }),
-        ["<C-j>"] = cmp.mapping.select_next_item({
-          behavior = cmp.SelectBehavior.Insert
-        }),
-        ["<C-n>"] = cmp.mapping.select_next_item({
-          behavior = cmp.SelectBehavior.Insert
-        }),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item({
-          behavior = cmp.SelectBehavior.Insert
-        }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({
-          behavior = cmp.SelectBehavior.Insert
-        }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({
-          behavior = cmp.SelectBehavior.Insert
-        }),
-      }),
-      sources = cmp.config.sources(
-        {
-          { name = "nvim_lsp" },
-        },
-        {
-          { name = "buffer" },
-        }
-      ),
-    })
+		local insert_keys = {
+			["<Tab>"] = {
+				i = cmp.mapping.confirm({
+					select = true
+				})
+			},
+			["<C-j>"] = {
+				i = cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+			["<C-n>"] = {
+				i = cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+			["<C-k>"] = {
+				i = cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+			["<C-p>"] = {
+				i = cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+		}
 
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" }
-      }
-    })
+		cmp.setup({
+			preselect = cmp.PreselectMode.None,
+			mapping = insert_keys,
+			sources = cmp.config.sources(
+				{
+					{ name = "nvim_lsp" },
+				},
+				{
+					{ name = "buffer" },
+				}
+			),
+		})
 
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" }
-      }, {
-        { name = "cmdline" }
-      }),
-      matching = { disallow_symbol_nonprefix_matching = false }
-    })
-  end,
+		local cmd_keys = {
+			["<Tab>"] = {
+				c = cmp.mapping.confirm({
+					select = true
+				})
+			},
+			["<C-j>"] = {
+				c = cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+			["<C-n>"] = {
+				c = cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+			["<C-k>"] = {
+				c = cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+			["<C-p>"] = {
+				c = cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select
+				})
+			},
+		}
+
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmd_keys,
+			sources = {
+				{ name = "buffer" }
+			}
+		})
+
+		cmp.setup.cmdline(":", {
+			mapping = cmd_keys,
+			sources = cmp.config.sources({
+				{ name = "path" }
+			}, {
+				{ name = "cmdline" }
+			}),
+			matching = { disallow_symbol_nonprefix_matching = false }
+		})
+	end,
 }
