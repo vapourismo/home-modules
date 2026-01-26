0579e36b27ee1220412ca719d247c79758097430
Ole Krüger <git@ole.lol>
2026-01-26 22:40:43 +0000

    NeoVim: Command to insert a word at cursor using Flash plugin

--- neovim/lua/plugins/flash.lua
+++ neovim/lua/plugins/flash.lua
@@ -34,6 +34,31 @@ return {
             })
         end)
 
+        vim.keymap.set("", "<D-f>", function()
+            flash.jump({
+                labels = "hjklionmasdfqwec",
+                label = {
+                    uppercase = false,
+                    before = true,
+                    after = false,
+                    style = "overlay",
+                },
+                pattern = ".",
+                search = {
+                    mode = function(pattern)
+                        if pattern:sub(1, 1) == "." then
+                            pattern = pattern:sub(2)
+                        end
+                        return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                    end,
+                },
+                jump = {
+                    autojump = false,
+                },
+            })
+            vim.cmd('normal viw"py"pp')
+        end)
+
         vim.keymap.set("", "mt", function()
             flash.treesitter({
                 label = { rainbow = { enabled = true } }
