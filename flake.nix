1d28efa40c8bb1a776ea89b16e8ceac2ab203da4
Ole Krüger <git@ole.lol>
2026-02-15 11:10:04 +0000

    Keep Neovim and Zed in parallel

--- flake.nix
+++ flake.nix
@@ -73,7 +73,8 @@
                 };
 
                 ole = {
-                  slot1 = "${config.programs.zed-editor.package}/Applications/Zed Nightly.app";
+                  slot1 = "${config.home.homeDirectory}/.nix-profile/Applications/Neovide.app";
+                  slot2 = "${config.programs.zed-editor.package}/Applications/Zed Nightly.app";
                   slot3 = "/Applications/Brave Browser.app";
                   jj.sshSignKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7vlc902QXTseSF7NsFy3CouUnWFQWDFy1EvS0CRD5q";
                 };
@@ -140,7 +141,8 @@
                 };
 
                 ole = {
-                  slot1 = "${config.programs.zed-editor.package}/Applications/Zed Nightly.app";
+                  slot1 = "${config.home.homeDirectory}/.nix-profile/Applications/Neovide.app";
+                  slot2 = "${config.programs.zed-editor.package}/Applications/Zed Nightly.app";
                   slot3 = "/Applications/Brave Browser.app";
                   slot4 = "/Applications/Slack.app";
                   slot5 = "/Applications/Linear.app";
