a85b1ccad63a7ea620bcac8a05d4c9a840a3f08c
Ole Krüger <git@ole.lol>
2026-02-17 14:36:41 +0000

    NeoVim: Configure CodeDiff

--- neovim/lua/plugins/diff.lua
+++ neovim/lua/plugins/diff.lua
@@ -1,5 +1,14 @@
 return {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+        diff = {
+            cycle_next_hunk = false,
+            cycle_next_file = false,
+        },
+        explorer = {
+            position = "bottom",
+        }
+    },
     cmd = "CodeDiff",
 }
