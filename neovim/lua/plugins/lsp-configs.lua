00d919cf27f3fbb9e16e1e2ba35946d54f6cdd1c
Ole Krüger <git@ole.lol>
2026-03-13 23:28:45 +0000

    NeoVim: Move DropBar to own plugin file

--- neovim/lua/plugins/lsp-configs.lua
+++ neovim/lua/plugins/lsp-configs.lua
@@ -1,95 +1,83 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local default_ra_before_init = vim.lsp.config["rust_analyzer"].before_init
-            vim.lsp.config("rust_analyzer", {
-                -- cmd = { "nix", "run", "github:NixOS/nixpkgs#rust-analyzer", "--" },
-                cmd = { "rust-analyzer" },
-                inlay_hints = { enabled = true },
-                capabilities = {
-                    textDocument = {
-                        completion = {
-                            completionItem = {
-                                snippetSupport = false
-                            }
+    "neovim/nvim-lspconfig",
+    config = function()
+        local default_ra_before_init = vim.lsp.config["rust_analyzer"].before_init
+        vim.lsp.config("rust_analyzer", {
+            -- cmd = { "nix", "run", "github:NixOS/nixpkgs#rust-analyzer", "--" },
+            cmd = { "rust-analyzer" },
+            inlay_hints = { enabled = true },
+            capabilities = {
+                textDocument = {
+                    completion = {
+                        completionItem = {
+                            snippetSupport = false
                         }
                     }
-                },
-                settings = {
-                    ["rust-analyzer"] = {
-                        check = { command = "clippy" },
-                        imports = { granularity = { enforce = true } },
-                        cargo = { targetDir = true },
-                    }
-                },
-                before_init = function(params, config)
-                    local ra_config_path = config.root_dir .. "/.rust-analyzer.json"
-                    local ra_config_file = io.open(ra_config_path, "r")
-                    if not ra_config_file then
-                        vim.notify(
-                            "Using default rust-analyzer config (" ..
-                            ra_config_path .. " not found)",
-                            vim.log.levels.INFO
-                        )
-
-                        return
-                    end
-
-                    local ra_config = ra_config_file:read("*a")
-                    ra_config_file:close()
-                    ra_config = vim.json.decode(ra_config)
-
+                }
+            },
+            settings = {
+                ["rust-analyzer"] = {
+                    check = { command = "clippy" },
+                    imports = { granularity = { enforce = true } },
+                    cargo = { targetDir = true },
+                }
+            },
+            before_init = function(params, config)
+                local ra_config_path = config.root_dir .. "/.rust-analyzer.json"
+                local ra_config_file = io.open(ra_config_path, "r")
+                if not ra_config_file then
                     vim.notify(
-                        "Using additional rust-analyzer config from " .. ra_config_path,
+                        "Using default rust-analyzer config (" ..
+                        ra_config_path .. " not found)",
                         vim.log.levels.INFO
                     )
 
-                    config.settings["rust-analyzer"] = vim.tbl_deep_extend(
-                        "force",
-                        config.settings["rust-analyzer"],
-                        ra_config
-                    )
+                    return
+                end
 
-                    default_ra_before_init(params, config)
-                end,
-            })
-            vim.lsp.enable("rust_analyzer")
+                local ra_config = ra_config_file:read("*a")
+                ra_config_file:close()
+                ra_config = vim.json.decode(ra_config)
 
-            -- vim.lsp.config("nil_ls", {
-            -- 	settings = {
-            -- 		["nil"] = {
-            -- 			formatting = { command = { "nixfmt" } }
-            -- 		}
-            -- 	}
-            -- })
-            -- vim.lsp.enable("nil_ls")
+                vim.notify(
+                    "Using additional rust-analyzer config from " .. ra_config_path,
+                    vim.log.levels.INFO
+                )
 
-            vim.lsp.config("buck2", {
-                cmd = { "buckle", "lsp" }
-            })
-            vim.lsp.enable("buck2")
+                config.settings["rust-analyzer"] = vim.tbl_deep_extend(
+                    "force",
+                    config.settings["rust-analyzer"],
+                    ra_config
+                )
 
-            vim.lsp.enable("nixd")
-            vim.lsp.enable("lua_ls")
-            vim.lsp.enable("taplo")
-            vim.lsp.enable("ocamllsp")
-            vim.lsp.enable("pylsp")
-            vim.lsp.enable("gopls")
-            vim.lsp.enable("openscad_lsp")
-            vim.lsp.enable("typos_lsp")
-            vim.lsp.enable("ts_ls")
-            vim.lsp.enable("jsonls")
-        end,
-    },
+                default_ra_before_init(params, config)
+            end,
+        })
+        vim.lsp.enable("rust_analyzer")
 
-    {
-        "Bekaboo/dropbar.nvim",
-        config = function()
-            local dropbar_api = require("dropbar.api")
-            vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-            vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-            vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-        end
-    }
+        -- vim.lsp.config("nil_ls", {
+        -- 	settings = {
+        -- 		["nil"] = {
+        -- 			formatting = { command = { "nixfmt" } }
+        -- 		}
+        -- 	}
+        -- })
+        -- vim.lsp.enable("nil_ls")
+
+        vim.lsp.config("buck2", {
+            cmd = { "buckle", "lsp" }
+        })
+        vim.lsp.enable("buck2")
+
+        vim.lsp.enable("nixd")
+        vim.lsp.enable("lua_ls")
+        vim.lsp.enable("taplo")
+        vim.lsp.enable("ocamllsp")
+        vim.lsp.enable("pylsp")
+        vim.lsp.enable("gopls")
+        vim.lsp.enable("openscad_lsp")
+        vim.lsp.enable("typos_lsp")
+        vim.lsp.enable("ts_ls")
+        vim.lsp.enable("jsonls")
+    end,
 }
