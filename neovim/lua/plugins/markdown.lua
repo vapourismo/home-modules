e9e74d30fb227fa072aecd085f1b6f4d87cde4d7
Ole Krüger <git@ole.lol>
2026-03-16 21:13:39 +0000

    NeoVim: Slim down Markdown config

--- neovim/lua/plugins/markdown.lua
+++ neovim/lua/plugins/markdown.lua
@@ -5,31 +5,12 @@ return {
     },
     ft = { "markdown", "opencode_output" },
     opts = {
-        anti_conceal = { enabled = false },
-        file_types = { "markdown", "opencode_output" },
         heading = {
-            sign = false,
-            position = "overlay",
             icons = { "█ ", "██ ", "███ ", "████ ", "█████ ", "██████ " },
-            width = "full",
         },
         code = {
             sign = false,
             style = "normal",
         },
-        checkbox = {
-            unchecked = {
-                icon = "",
-            },
-            checked = {
-                icon = "",
-            },
-        },
-        quote = {
-            icon = "▏",
-        },
-        pipe_table = {
-            style = "normal",
-        }
     },
 }
