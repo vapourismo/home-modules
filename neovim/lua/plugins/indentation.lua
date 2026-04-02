89e2b1d5e6c67fc88e52512a2c400f5e4985c37d
Ole Krüger <git@ole.lol>
2026-04-02 22:13:53 +0100

    NeoVim: Detect indentation

---
+++ neovim/lua/plugins/indentation.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {
+            indent = {
+                char = "▏"
+            },
+            scope = {
+                enabled = false
+            }
+        }
+    },
+    {
+        "Darazaki/indent-o-matic",
+        opts = {},
+    }
+}
