23711d532517ee6dc454961dbe04249ea37c99b6
Ole Krüger <git@ole.lol>
2026-04-02 22:39:07 +0100

    Neovide: Shorten position animation

--- neovim/lua/config/settings.lua
+++ neovim/lua/config/settings.lua
@@ -52,7 +52,7 @@ if vim.g.neovide then
     set_font(1)
 
     vim.g.neovide_scroll_animation_length = 0
-    vim.g.neovide_position_animation_length = 0.1
+    vim.g.neovide_position_animation_length = 0
     vim.g.neovide_cursor_animation_length = 0
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_fullscreen = true
