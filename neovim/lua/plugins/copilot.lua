4727e69b47545e1ec1b1b5f8013b9006d48dc933
Ole Krüger <git@ole.lol>
2026-04-15 18:27:19 +0100

    NeoVim: Rethink copilot keybindings

--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -1,48 +1,88 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        dependencies = {
-            "copilotlsp-nvim/copilot-lsp",
+    "zbirenbaum/copilot.lua",
+    dependencies = {
+        "copilotlsp-nvim/copilot-lsp",
+        init = function()
+            vim.g.copilot_nes_debounce = 500
+        end,
+    },
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+        suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            keymap = {
+                accept = false,
+                accept_word = false,
+                accept_line = false,
+            },
         },
-        cmd = "Copilot",
-        event = "InsertEnter",
-        opts = {
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                keymap = {
-                    accept_word = "<M-S-L>",
-                    accept_line = "<M-S-J>",
+        nes = {
+            enabled = true,
+            auto_trigger = true,
+            keymap = {
+                accept_and_goto = false,
+                accept = false,
+                dismiss = false,
+            },
+        },
+        filetypes = {
+            opencode = false,
+            opencode_output = false,
+        },
+        panel = { enabled = false },
+        should_attach = function(_, bufname)
+            if string.match(bufname, "env") then
+                return false
+            end
+
+            return true
+        end,
+        server_opts_overrides = {
+            settings = {
+                telemetry = {
+                    telemetryLevel = "off",
                 },
             },
-            nes = {
-                enabled = true,
-                auto_trigger = true,
-                keymap = {
-                    accept_and_goto = "<Space><M-l>",
-                    accept = false,
-                    dismiss = false,
-                },
-            },
-            filetypes = {
-                opencode = false,
-                opencode_output = false,
-            },
-            panel = { enabled = false },
-            should_attach = function(_, bufname)
-                if string.match(bufname, "env") then
-                    return false
+        }
+    },
+    keys = {
+        {
+            "<M-l>",
+            function()
+                local suggestion = require("copilot.suggestion")
+
+                if suggestion.is_visible() then
+                    suggestion.accept()
+                    return
                 end
 
-                return true
+                local nes = require("copilot-lsp.nes")
+
+                if not nes.walk_cursor_start_edit() then
+                    if nes.apply_pending_nes() then
+                        nes.walk_cursor_end_edit()
+                    end
+                end
             end,
-            server_opts_overrides = {
-                settings = {
-                    telemetry = {
-                        telemetryLevel = "off",
-                    },
-                },
-            }
+            mode = { "n", "i" }
+        },
+        {
+            "<M-S-L>",
+            function()
+                local suggestion = require("copilot.suggestion")
+                suggestion.accept_word()
+            end,
+            mode = { "n", "i" }
+        },
+        {
+            "<M-n>",
+            function()
+                local suggestion = require("copilot.suggestion")
+                suggestion.accept_line()
+            end,
+            mode = { "n", "i" }
         }
     },
 }
