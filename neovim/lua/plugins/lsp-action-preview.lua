1030240c79573243eb7f898afb60840369dbee8f
Ole Krüger <git@ole.lol>
2026-04-20 19:20:51 +0100

    NeoVim: Make actions preview diff context larger

--- neovim/lua/plugins/lsp-action-preview.lua
+++ neovim/lua/plugins/lsp-action-preview.lua
@@ -2,6 +2,14 @@ return {
     "aznhe21/actions-preview.nvim",
     config = function()
         local actions_preview = require("actions-preview")
+
+        actions_preview.setup({
+            diff = {
+                ctxlen = 5,
+            },
+            backend = { "snacks" }
+        })
+
         vim.keymap.set("", "<Space>a", actions_preview.code_actions)
     end,
 }
