768762f66e7a200437b6c1bdc256d40ac13902cd
Ole Krüger <git@ole.lol>
2026-05-27 15:14:45 +0100

    NeoVim: Move Noice config to separate file

--- neovim/lua/plugins/theme.lua
+++ neovim/lua/plugins/theme.lua
@@ -101,31 +101,4 @@ return {
             vim.cmd("colorscheme warm-burnout-dark")
         end,
     },
-
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            popupmenu = {
-                backend = "cmp"
-            },
-            lsp       = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
-            presets   = {
-                bottom_search = true,
-                command_palette = true,
-                long_message_to_split = true,
-                inc_rename = false,
-                lsp_doc_border = false,
-            },
-        },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-        }
-    }
 }
