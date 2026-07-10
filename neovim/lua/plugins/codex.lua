b6b87b6917a22c4d0461d7429f009f5769e5a47c
Ole Krüger <git@ole.lol>
2026-07-10 11:07:21 +0100

    NeoVim: Add key map for adding file references to Codex

--- neovim/lua/plugins/codex.lua
+++ neovim/lua/plugins/codex.lua
@@ -2,6 +2,7 @@ return {
     "vapourismo/codex.nvim",
     opts = {},
     keys = {
-        { "<D-r>", function() require("codex").toggle() end, mode = { "n", "v", "i", "t" } },
+        { "<D-r>", function() require("codex").toggle() end,    mode = { "n", "v", "i", "t" } },
+        { "<D->>", function() require("codex").reference() end, mode = { "v" } },
     },
 }
