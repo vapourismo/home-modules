4e6e39c5abc7ca34bbdecc4113200961612b5e78
Ole Krüger <git@ole.lol>
2026-07-15 17:31:08 +0100

    NeoVim: Add JJ workspace manager

---
+++ neovim/lua/plugins/jjwsm.lua
@@ -0,0 +1,9 @@
+return {
+    "vapourismo/jjwsm.nvim",
+    config = false,
+    keys = {
+        { "<Space>ws", "<cmd>Jjwsm switch<cr>" },
+        { "<Space>wn", "<cmd>Jjwsm new<cr>" },
+        { "<Space>wd", "<cmd>Jjwsm delete<cr>" },
+    },
+}
