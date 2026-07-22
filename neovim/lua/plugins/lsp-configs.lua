85d18545be5b2ec26dd352f46c1a04ac926562cc
Ole Krüger <git@ole.lol>
2026-07-22 16:26:54 +0100

    NeoVim: Disable copilot lsp

--- neovim/lua/plugins/lsp-configs.lua
+++ neovim/lua/plugins/lsp-configs.lua
@@ -112,14 +112,14 @@ return {
         })
         vim.lsp.enable("buck2")
 
-        vim.lsp.config("copilot", {
-            settings = {
-                telemetry = {
-                    telemetryLevel = "off",
-                },
-            },
-        })
-        vim.lsp.enable("copilot")
+        -- vim.lsp.config("copilot", {
+        --     settings = {
+        --         telemetry = {
+        --             telemetryLevel = "off",
+        --         },
+        --     },
+        -- })
+        -- vim.lsp.enable("copilot")
 
         vim.lsp.enable("nixd")
         vim.lsp.enable("lua_ls")
