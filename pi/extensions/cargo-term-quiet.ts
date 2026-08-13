9852916f4f5559c880f52d59f35e78e8b143f84c
Ole Krüger <git@ole.lol>
2026-08-13 22:43:23 +0100

    Pi: Clear more Rust/Cargo related environment variables

--- pi/extensions/cargo-term-quiet.ts
+++
@@ -1,5 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function setCargoTermQuiet(_pi: ExtensionAPI): void {
-  process.env.CARGO_TERM_QUIET = "true";
-}
