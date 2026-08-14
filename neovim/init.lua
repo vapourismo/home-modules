25760aabb3294ac13610cc901d8792593760e8b8
Ole Krüger <git@ole.lol>
2026-08-14 17:45:29 +0100

    NeoVim: Use winbars.lua plugin

--- neovim/init.lua
+++ neovim/init.lua
@@ -13,8 +13,5 @@ require("config.usercmds")
 -- Autocmds
 require("config.autocmds")
 
--- Winbar
-require("config.winbar")
-
 -- Lazy plugin manager
 require("config.lazy")
---
+++ neovim/lua/plugins/winbars.lua
@@ -0,0 +1,5 @@
+return {
+    "vapourismo/winbars.lua",
+    dir = "/Users/ole/Workspaces/winbars.nvim",
+    opts = {},
+}
