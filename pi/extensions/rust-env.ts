43b0e24fa12e3f6b024b551d05322dbc7ccb7ffb
Ole Krüger <git@ole.lol>
2026-08-15 17:22:43 +0100

    Pi: Give commands more access to caches

--- pi/extensions/rust-env.ts
+++ pi/extensions/rust-env.ts
@@ -3,12 +3,4 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 export default function setCargoTermQuiet(_pi: ExtensionAPI): void {
   // Make sure Cargo does not pollute the context with unnecessary output.
   process.env.CARGO_TERM_QUIET = "true";
-
-  // If kache or sccache is used, disable it.
-  if (
-    process.env.RUSTC_WRAPPER?.includes("kache") ||
-    process.env.RUSTC_WRAPPER?.includes("sccache")
-  ) {
-    process.env.RUSTC_WRAPPER = "";
-  }
 }
