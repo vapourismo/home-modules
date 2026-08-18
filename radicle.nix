f09bbaf5cc8f0ee3d86e76fceebb77703f471186
Ole Krüger <git@ole.lol>
2026-08-18 14:49:49 +0100

    Radicle: Connect to jenkins1

--- radicle.nix
+++ radicle.nix
@@ -4,6 +4,12 @@
     enable = true;
     settings = {
       node.alias = "ole";
+      node.connect = [
+        "z6MkeiacB2PjqgAc9Hyi1oGW5U43RRXVMBBgJ3586gYdXTtU@46.224.149.156:8776"
+      ];
+      preferredSeeds = [
+        "z6MkeiacB2PjqgAc9Hyi1oGW5U43RRXVMBBgJ3586gYdXTtU@46.224.149.156:8776"
+      ];
     };
   };
 }
