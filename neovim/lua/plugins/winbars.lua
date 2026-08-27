b37ef0b74c5fe36fae127342bb51f5c72fccefcf
Ole Krüger <git@ole.lol>
2026-08-27 16:13:31 +0100

    NeoVim: Make win bar context separators more obvious

--- neovim/lua/plugins/winbars.lua
+++ neovim/lua/plugins/winbars.lua
@@ -2,6 +2,12 @@ return {
     "vapourismo/winbars.lua",
     dir = "/Users/ole/Workspaces/winbars.nvim",
     opts = {
+        separator = "  ",
+        absolute_prefix = " ",
+        context = {
+            path_separator = " 󰅂 ",
+            separator = " 󰅂 ",
+        },
         icons = {
             modified = " ",
         },
