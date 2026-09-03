c892d001494f1416d0e8dc33d52b29c280afdf00
Ole Krüger <git@ole.lol>
2026-09-03 14:20:46 +0100

    NeoVim: Disable folding for now

--- neovim/lua/config/settings.lua
+++ neovim/lua/config/settings.lua
@@ -27,8 +27,7 @@ vim.opt.shiftwidth       = 4
 vim.opt.tabstop          = 4
 vim.opt.expandtab        = true
 
-vim.opt.foldmethod       = "expr"
-vim.opt.foldexpr         = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldmethod       = "manual"
 vim.opt.foldtext         = ""
 vim.opt.foldcolumn       = "0"
 vim.opt.foldlevel        = 99
