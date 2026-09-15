b54dabc80bf40527d283a6e5c634c85025ce2a12
Ole Krüger <git@ole.lol>
2026-09-15 11:06:29 +0100

    NeoVim: Keep RUSTC_WRAPPER for rust-analyzer

--- neovim/lua/plugins/lsp-configs.lua
+++ neovim/lua/plugins/lsp-configs.lua
@@ -4,9 +4,6 @@ return {
         local default_ra_before_init = vim.lsp.config["rust_analyzer"].before_init
         vim.lsp.config("rust_analyzer", {
             cmd = { "rust-analyzer" },
-            cmd_env = {
-                RUSTC_WRAPPER = "",
-            },
             inlay_hints = { enabled = true },
             settings = {
                 ["rust-analyzer"] = {
